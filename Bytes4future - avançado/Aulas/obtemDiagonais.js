df84e8c1c1a2126920621ed8d2dbe24a98b0456b
Caweca105 <lemos.diogo105@gmail.com>
2021-05-25 11:07:11 +0100

    a

--- Bytes4future - avançado/Aulas/obtemDiagonais.js
+++ Bytes4future - avançado/Aulas/obtemDiagonais.js
@@ -1,35 +1,42 @@
 console.log(
-obtemDiagonais([
-    [1, 2, 3],
-    [1, 2]
-])
+    obtemDiagonais([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9] 
+    ]) // [[7], [4, 8], [1, 5, 9], [2, 6], [3]]
 )
 
 console.log(
-obtemDiagonais([
-    [1, 2, 3],
-    [1, 2]
-])
+    obtemDiagonais([
+        [1, 2],
+        [1, 2] 
+    ]) // [[1], [1, 2], [2]]
 )
 
 function obtemDiagonais(matriz) {
     // Obtem diagonal principal
-    let diagonais = [diagonalPrincipal(matriz, 0, 0)];
+    let diagonais = [
+        diagonalPrincipal(matriz, 0, 0)
+    ]
 
-    for(let i = 1; i < matriz.length; i++) {
-        diagonais = [diagonalPrincipal(matriz, i, 0)]
-        .concat(diagonais)
-        .concat([diagonPrincipal(matriz, 0 , i)])
+    for (let i = 1; i < matriz.length; i++) {
+        diagonais = [
+            diagonalPrincipal(matriz, i, 0)
+        ]
+            .concat(diagonais)
+            .concat([
+                diagonalPrincipal(matriz, 0, i)
+            ])
     }
 
-    return diagonalPrincipal(matriz, 0 , 0)
+    return diagonais;
 }
 
-function diagonalPrincipal(matriz, linha, coluna){
+function diagonalPrincipal(matriz, linha, coluna) {
     const diagonal = []
-    for(let i = 0; i < matriz.length - linha; i++){
-        const elemento = matriz[linha + i][coluna + i]
-        if(elemento){
+    for(let i = 0; i < matriz.length - linha; i++) {
+        const elemento = matriz[linha + i][coluna + i];
+        if (elemento) {
             diagonal.push(elemento)
         }
     }
