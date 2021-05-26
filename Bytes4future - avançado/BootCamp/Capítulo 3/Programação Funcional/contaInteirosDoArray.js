d8fa9476227284a2fe4b5d9207a5bedb5707af4b
Caweca105 <lemos.diogo105@gmail.com>
2021-05-26 14:23:12 +0100

    a

--- Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/contaInteirosDoArray.js
+++ Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/contaInteirosDoArray.js
@@ -1,5 +1,3 @@
 function contaInteirosDoArray(array){
     // escreve aqui a função
-    if (array.length === 0) return 0;
-    if (array[0]) return contaInteirosDoArray(Math.round(array))
 }
---
+++ Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/dist/contaInteirosDoArray.dev.js
@@ -0,0 +1,4 @@
+"use strict";
+
+function contaInteirosDoArray(array) {// escreve aqui a função
+}
