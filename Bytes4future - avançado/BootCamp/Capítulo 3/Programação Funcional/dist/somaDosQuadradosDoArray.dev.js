d412330d5c4b4a8252c182bd2c0498347d7deb89
Caweca105 <lemos.diogo105@gmail.com>
2021-05-30 18:17:45 +0100

    a

--- Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/dist/somaDosQuadradosDoArray.dev.js
+++ Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/dist/somaDosQuadradosDoArray.dev.js
@@ -3,5 +3,5 @@
 function somaDosQuadradosDoArray(array) {
   // escreve aqui a função
   if (array.length === 0) return 0;
-  return Math.pow(array[0], 2) + somaDoArray(array.slice(1));
+  return Math.pow(array[0], 2) + somaDosQuadradosDoArray(array.slice(1));
 }
--- Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/somaDosQuadradosDoArray.js
+++ Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional/somaDosQuadradosDoArray.js
@@ -1,5 +1,5 @@
 function somaDosQuadradosDoArray(array){
     // escreve aqui a função
     if (array.length === 0) return 0;
-    return ((array[0] ** 2) + somaDoArray(array.slice(1)))
+    return ((array[0] ** 2) + somaDosQuadradosDoArray(array.slice(1)))
 }
