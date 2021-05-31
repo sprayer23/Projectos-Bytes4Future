245a52c90d089435edd71f77bcc55028a0ac4ff4
Caweca105 <lemos.diogo105@gmail.com>
2021-05-31 09:09:33 +0100

    l

--- Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional em Javascript/encontraPrimo.js
+++ Bytes4future - avançado/BootCamp/Capítulo 3/Programação Funcional em Javascript/encontraPrimo.js
@@ -6,7 +6,7 @@ function encontraPrimo(array){
     function primo(num) {
     // verifica se o numero digitado é "1", que não é primo
      if(num!=1){
-      for (var i = 2; i < num; i++)
+      for (let i = 2; i < num; i++)
         if (num % i == 0) return false;
       return num !== 1;
     }
