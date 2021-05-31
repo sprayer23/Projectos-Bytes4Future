ba745e2e1776b2d27c0c87f4beae202acfdbedd0
Caweca105 <lemos.diogo105@gmail.com>
2021-05-31 11:31:53 +0100

    New folders created

---
+++ Bytes4future - avançado/BootCamp/Exercícios/Front-end/Capítulo 2/Dom e eventos/DomEEventos1.js
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercício</title>
+</head>
+<body>
+    <section class="contador">
+        <h2 id="valor">0</h2>
+        <button id="incrementar">Incrementar</button>
+        <button id="decrementar">Decrementar</button>
+    </section>
+<script>
+    // Completar
+</script>
+</body>
+</html>
